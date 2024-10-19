82b5b9cdc5f64417482a4ebf9f154e3d5d0e9945
aamna-ansari <aamnansari29@gmail.com>
2024-10-19 07:18:28 +0500

    Added

---
+++ 08_sign-up_form/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            tomato: 'hsl(4, 100%,67%)', // Primary Color
+            DSlateGrey: 'hsl(235, 18%, 26%)', // bgColor
+            Gray: 'hsl(231, 7%, 60%);', // emailinputfield Color
+            White: 'hsl(0, 0%, 100%)', // btnText Color
+        }
+      }
+    }
+  }

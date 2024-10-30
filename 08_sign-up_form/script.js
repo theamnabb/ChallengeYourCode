4828ef4591b9a06c9390807e0e750be63496f1c9
aamna-ansari <aamnansari29@gmail.com>
2024-10-30 20:36:20 +0500

    variable created

--- 08_sign-up_form/script.js
+++ 08_sign-up_form/script.js
@@ -1,1 +1,6 @@
-let emailField = document.getElementById ('emailOutput');
+let content1 = document.getElementById('content1') // first card Access
+let content2 = document.getElementById('content2') // Second card Access
+let emailInput = document.getElementById('email') // inputfield Email
+let btn1 = document.getElementById('btn1') // First Button aaccess
+let btn2 = document.getElementById('btn2') // Second Button aaccess
+let emailField = document.getElementById ('emailOutput'); // for span Email update 

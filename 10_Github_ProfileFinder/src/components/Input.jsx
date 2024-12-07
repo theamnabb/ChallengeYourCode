006d0d4a014099188b4b01435e1bd281e3c024c2
aamna-ansari <aamnansari29@gmail.com>
2024-12-07 21:53:41 +0500

    GPF

--- 10_Github_ProfileFinder/src/components/Input.jsx
+++ 10_Github_ProfileFinder/src/components/Input.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import { FaSearch } from "react-icons/fa";
+import React, { useEffect, useState } from 'react'
 
 const Input = () => {
+  const [userName, setUserName]= useState ('aamna-ansari')
+
+  useEffect(()=>{
+
+  },[]);
+
+  const handleSearch = () => {
+    // Trigger the card re-render by passing the username
+  };
+
   return (
     <>
 
@@ -9,12 +18,16 @@ const Input = () => {
         <input className=' p-4 pr-12 bg-gray-800 shadow-md rounded-lg outline-none text-gray-400 w-full sm:w-3/4 md:w-1/2 mt-5 placeholder:text-gray-500 '
         type="text"
         placeholder='Search Github username....'
+        onChange={e => setUserName(e.target.value)}
          />
-         <span className="text-white absolute right-0 top-[40%] sm:right-1/4 px-8 cursor-pointer" >
+         <span
+         onClick={handleSearch}
+        className="text-white absolute right-0 top-[40%] sm:right-1/4 px-8 cursor-pointer" >
          <i className="fas fa-search" ></i>
          </span>
    
     </div>
+    <Card username={userName} />
     </>
   )
 }

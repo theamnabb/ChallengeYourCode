3c6059ca3177fe869fe1129be44e96de9c4dc496
aamna-ansari <aamnansari29@gmail.com>
2024-12-10 22:23:49 +0500

    GPF

--- 10_Github_ProfileFinder/src/components/Input.jsx
+++ 10_Github_ProfileFinder/src/components/Input.jsx
@@ -1,34 +1,25 @@
-import { useState } from "react";
-
-const Input = () => {
-  
-  const [username, setUsername] = useState(''); // username (to store the input value).
-  const [userData, setUserData] = useState(null); // userData (to store the fetched data)
-  
+const Input = ({ username, setUsername, fetchData }) => {
   const handleUsername = (e) => {
-    console.log(e.target.value)
-    setUsername(e.target.value);   
-};
+    console.log(e.target.value);
+    setUsername(e.target.value);
+  };
 
-// 
-  
   return (
     <>
-    <div className='w-full flex justify-center relative pb-4'>
-        <input className=' p-4 pr-12 dark:bg-gray-800 bg-white drop-shadow-2xl rounded-lg outline-none text-gray-400 w-full sm:w-3/4 md:w-1/2 mt-5 placeholder:text-gray-500 '
-        type="text"
-        value={username}
-        onChange={handleUsername}
-        placeholder='Search Github username....'
-         />
-         <span
-        className="dark:text-white  text-gray-600 absolute right-0 top-[40%] sm:right-1/4 px-8 cursor-pointer" >
-         <i className="fas fa-search" ></i>
-         </span>
-   
-    </div>
+      <div className="w-full flex justify-center relative pb-4">
+        <input
+          className=" p-4 pr-12 dark:bg-gray-800 bg-white drop-shadow-2xl rounded-lg outline-none text-gray-400 w-full sm:w-3/4 md:w-1/2 mt-5 placeholder:text-gray-500 "
+          type="text"
+          value={username}
+          onChange={handleUsername}
+          placeholder="Search Github username...."
+        />
+        <span className="dark:text-white  text-gray-600 absolute right-0 top-[40%] sm:right-1/4 px-8 cursor-pointer">
+          <i className="fas fa-search"></i>
+        </span>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
--- 10_Github_ProfileFinder/src/components/Main.jsx
+++ 10_Github_ProfileFinder/src/components/Main.jsx
@@ -1,25 +1,37 @@
-import Card from './Card'
-import  Input  from './Input'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import Input from "./Input";
 
 const Main = () => {
-//   const [username, setUsername] = useState(''); // username (to store the input value).
-//   const [userData, setUserData] = useState(null); // userData (to store the fetched data)
-  
-//   const handleUsername = (e) => {
-//     console.log(setUsername(e.target.value));
-    
-// };
+  const [username, setUsername] = useState(""); // username (to store the input value).
+  const [userData, setUserData] = useState(null); // userData (to store the fetched data)
 
-  
+  // 📍 FETCH DATA 📍
+
+  const fetchData = async () => {
+    try {
+      const respone = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = respone.json();
+      setUserData(data);
+    } catch (error) {
+      console.log("User Not Found");
+    }
+  };
+
+  useEffect(() => {
+    if (username) {
+      fetchData();
+    }
+  }, [username]);
+
   return (
     <>
-    <main className='p-4'>
-    <Input/>
-    <Card/>
-    </main>
+      <main className="p-4">
+        <Input username={username} setUsername={setUsername} fetchData={fetchData} />
+        <Card />
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;

47900c33a78704d0420059a8c5d43504f2dd545f
aamna-ansari <aamnansari29@gmail.com>
2024-12-10 21:56:56 +0500

    gpf

--- 10_Github_ProfileFinder/src/components/Input.jsx
+++ 10_Github_ProfileFinder/src/components/Input.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 
 const Input = () => {
+  
+  const [username, setUsername] = useState(''); // username (to store the input value).
+  const [userData, setUserData] = useState(null); // userData (to store the fetched data)
+  
+  const handleUsername = (e) => {
+    console.log(e.target.value)
+    setUsername(e.target.value);   
+};
+
+// 
   
   return (
     <>
     <div className='w-full flex justify-center relative pb-4'>
         <input className=' p-4 pr-12 dark:bg-gray-800 bg-white drop-shadow-2xl rounded-lg outline-none text-gray-400 w-full sm:w-3/4 md:w-1/2 mt-5 placeholder:text-gray-500 '
         type="text"
+        value={username}
+        onChange={handleUsername}
         placeholder='Search Github username....'
          />
          <span
--- 10_Github_ProfileFinder/src/components/Main.jsx
+++ 10_Github_ProfileFinder/src/components/Main.jsx
@@ -1,8 +1,17 @@
 import Card from './Card'
 import  Input  from './Input'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Main = () => {
+//   const [username, setUsername] = useState(''); // username (to store the input value).
+//   const [userData, setUserData] = useState(null); // userData (to store the fetched data)
+  
+//   const handleUsername = (e) => {
+//     console.log(setUsername(e.target.value));
+    
+// };
+
+  
   return (
     <>
     <main className='p-4'>
